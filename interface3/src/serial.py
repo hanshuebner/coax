34db32b5d04b5141eec14669fad1d81f1f956905
Hans Hübner <hans.huebner@gmail.com>
2025-10-20 08:51:56 +0200

    beginnings of (USB) serial protocol implementation

--- interface3/src/serial.py
+++ interface3/src/serial.py
@@ -2,6 +2,10 @@ import sys
 import micropython
 import struct
 
+HARDWARE_TYPE = "interface3"
+FIRMWARE_VERSION = "v0.0"
+MAX_FRAME_SIZE = 4300 # FIXME this does not belong here
+
 # SLIP Protocol Constants
 END = 0xC0      # 192 - Frame End
 ESC = 0xDB      # 219 - Frame Escape
@@ -103,13 +107,50 @@ def write_slip_frame(data):
 
     write_byte(END)
 
-def send_error(code, message):
+def error_response(code, message):
     encoded_message = message.encode()
 
     return struct.pack('>HBB', len(encoded_message), RESPONSE_ERROR, code) + encoded_message
 
-def cmd_reset():
+def cmd_reset(buf):
     return b'\x00\x03\x01\x32\x70\x00\x00'
+
+def cmd_info(buf):
+    query = buf[0]
+    if query == INFO_SUPPORTED_QUERIES:
+        return struct.pack(">HBBBBBB", 6,
+                           RESPONSE_OK,
+                           INFO_SUPPORTED_QUERIES,
+                           INFO_HARDWARE_TYPE,
+                           INFO_FIRMWARE_VERSION,
+                           INFO_MESSAGE_BUFFER_SIZE,
+                           INFO_FEATURES)
+                           
+    elif query == INFO_HARDWARE_TYPE:
+        encoded_message = HARDWARE_TYPE.encode()
+
+        return struct.pack('>HB', len(encoded_message),
+                           RESPONSE_OK) + encoded_message
+
+    elif query == INFO_FIRMWARE_VERSION:
+        encoded_message = FIRMWARE_VERSION.encode()
+
+        return struct.pack('>HB', len(encoded_message),
+                           RESPONSE_OK) + encoded_message
+
+    elif query == INFO_MESSAGE_BUFFER_SIZE:
+        return struct.pack('>HB>L', 4,
+                           RESPONSE_OK,
+                           MAX_FRAME_SIZE)
+
+    elif query == INFO_FEATURES:
+        return struct.pack('>HB', 0, RESPONSE_OK)
+
+    else:
+        return error_response(ERROR_INVALID_MESSAGE, "unknown query type")
+
+def cmd_transmit_receive(buf):
+    return coax.transact(buf)
 
 def main():
     sys.stderr.write("SLIP server starting\n")
@@ -124,9 +165,13 @@ def main():
         length, command, buf = unpack_message(frame)
 
         if command == COMMAND_RESET:
-            response = cmd_reset()
+            response = cmd_reset(buf)
+        elif command == COMMAND_INFO:
+            response = cmd_info(buf)
+        elif command == COMMAND_TRANSMIT_RECEIVE:
+            response = cmd_transmit_receive(buf)
         else:
-            response = send_error(ERROR_UNKNOWN_COMMAND, "command not (yet) implemented")
+            response = error_response(ERROR_UNKNOWN_COMMAND, "command not (yet) implemented")
 
         #sys.stderr.write("Data: {}\n".format(repr(frame)))
         write_slip_frame(response)
